42a24531216b0da49394477afbdacb5408fd5287
charlieabhishek <abcptech@gmail.com>
2019-04-19 12:28:56 +0530

    sync, async and qos examples

--- MultiThreading/MultiThreading/ViewController.swift
+++ MultiThreading/MultiThreading/ViewController.swift
@@ -12,9 +12,72 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        syncExample()
+//        asyncExample()
+//        deadLockExample1()
+        queuesWitQOS()
     }
-
+    
+    func queuesWitQOS(){
+        let queue1 = DispatchQueue(label: "com.multithreading.queue1",qos: .userInteractive)
+        let queue2 = DispatchQueue(label: "com.multithreading.queue2",qos: .userInitiated)
+        queue1.async {
+            for i in 0..<10{
+                print("💔",i)
+            }
+        }
+        queue2.async {
+            for i in 100..<110{
+                print("💙",i)
+            }
+        }
+    }
+    
+    func deadLockExample1(){
+        let queueAsync = DispatchQueue(label: "com.multithreading.queueAsync")
+        let queueSync  = DispatchQueue(label: "com.multithreading.queueSync")
+        
+        queueAsync.async {
+            for i in 1...1130{
+                print("📟 \(i)")
+            }
+        }
+        for i in 1...200{
+            print("☪️ \(i)")
+        }
+        queueAsync.sync {
+            for i in 1...100{
+                print("💛 \(i)")
+            }
+        }
+        
+    }
+    
+    func asyncExample(){
+        let queue = DispatchQueue(label: "com.multithreading.queue1")
+        queue.async {
+            for i in 0..<10{
+                print("🚒 \(i)")
+            }
+        }
+        
+        for i in 100..<110{
+            print("🚐 \(i)")
+        }
+    }
+    
+    func syncExample(){
+        let queue = DispatchQueue(label: "com.multithreading.queue")
+        queue.sync {
+            for i in 0..<10{
+                print("🚒 \(i)")
+            }
+        }
+        
+        for i in 100..<110{
+            print("🚐 \(i)")
+        }
+    }
 
 }
 
